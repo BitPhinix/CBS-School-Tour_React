3f284935ac02b3ddb8b80f04a09cd9f9d4be4dda
BitPhinix <meier.eric2@t-online.de>
2018-01-19 20:51:35 +0100

    Basic SVG implementation, I can´t use code from the old project 😭😭😭😭

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import AutoCompleteContainer from "./components/autoCompleteContainer";
 import Map from "./components/map";
 import NavigationSlider from "./components/navigationSlider";
 import SearchBar from "./components/searchBar";
 const Style = require("./app.css");
 
 class App extends React.Component<{}, {}> {
-
-    private container: AutoCompleteContainer;
-
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return(
             <div className="App" style={Style}>
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,12 +1,24 @@
-import * as React from "react";
+import * as React from "react"
+import {AutoSizer} from 'react-virtualized';
+import {ReactSVGPanZoom, TOOL_PAN} from "react-svg-pan-zoom";
 const Style = require("./map.css");
 
 class Map extends React.Component<{}, {}> {
 
     render() {
         return(
-            <div className="Map" style={Style}>
-                Map
+            <div style={Style} className="mapContainer">
+                <AutoSizer>
+                    {(({width, height}) => width === 0 || height === 0 ? null : (
+                        <ReactSVGPanZoom
+                            className="map"
+                            width={width}
+                            height={height}
+                            tool={TOOL_PAN}>
+                            <svg width={1440} height={1440}/>
+                        </ReactSVGPanZoom>
+                    ))}
+                </AutoSizer>
             </div>
         );
     }
