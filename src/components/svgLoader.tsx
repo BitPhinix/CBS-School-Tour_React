e811522d55427118167c95783724e5bed4fe23c6
BitPhinix <meier.eric2@t-online.de>
2018-01-24 16:33:15 +0100

    This took a while since i was looking for a working lib with svg pan/zoom functionality (which i didn´t found)

--- src/components/svgLoader.tsx
+++ src/components/svgLoader.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 
-class SvgLoader extends React.Component<{}, {htmlString: string}> {
+class SvgLoader extends React.Component<{}, {inlineElements: string, scale: number, translation: {x: number, y: number}, rotation: number}> {
+
+    svg;
+    mousePressed: boolean;
+    lastMouseXPos: number;
+    lastMouseYPos: number;
 
     constructor(props) {
         super(props);
 
         //Default state
-        this.state = {htmlString: ""};
+        this.state = {inlineElements: "", scale: 1, rotation: 0, translation: {x: 0, y: 0}};
+
+        //Add event listener
+        document.body.addEventListener("mouseup", () => this.mousePressed = false);
+        document.body.addEventListener("mouseleave", () => this.mousePressed = false);
+        document.body.addEventListener("mousemove", (event) => this.onMouseMove(event));
     }
 
     componentDidMount() {
@@ -20,8 +30,12 @@ class SvgLoader extends React.Component<{}, {htmlString: string}> {
 
         //THis function will be called when call is complete
         xmlRequest.onreadystatechange = () => {
-            //Set htmlString
-            this.setState({htmlString: xmlRequest.responseText});
+
+            //Remove enclosing svg tags (just get content)
+            const svgContent = xmlRequest.responseText.replace(new RegExp("<svg.*>|<\/svg.*>"), "");
+
+            //Set inlineElements
+            this.setState({inlineElements: svgContent});
         };
 
         //Make call (async)
@@ -29,9 +43,58 @@ class SvgLoader extends React.Component<{}, {htmlString: string}> {
         xmlRequest.send(null);
     }
 
+    onWheel(event) {
+        //Calculate zoom and update state
+        this.setState({scale: this.state.scale + event.deltaY / 400});
+    }
+
+    onMouseMove(event) {
+        //If mouse isn´t pressed
+        if(!this.mousePressed)
+            return;
+
+        //Calculate movement deltas under consideration of the scale
+        const deltaX = (this.lastMouseXPos - event.screenX) / this.state.scale;
+        const deltaY = (this.lastMouseYPos - event.screenY) / this.state.scale;
+
+        //Update state
+        this.setState({translation: {
+                x: this.state.translation.x - deltaX,
+                y: this.state.translation.y - deltaY
+            }});
+
+        //Set lastMouseXPos, lastMouseYPos for delta calculations
+        this.lastMouseXPos = event.screenX;
+        this.lastMouseYPos = event.screenY;
+
+        return false;
+    }
+
+    onMouseDown(event) {
+        //Set mousePressed to true
+        this.mousePressed = true;
+
+        //Set lastMouseXPos, lastMouseYPos for delta calculations
+        this.lastMouseXPos = event.screenX;
+        this.lastMouseYPos = event.screenY;
+    }
+
     render() {
         return (
-            <div dangerouslySetInnerHTML={{__html: this.state.htmlString}}/>
+            <svg preserveAspectRatio="none" width="100%" height="100%" ref={(svg) => this.svg = svg}
+                onWheel={(event) => this.onWheel(event)}
+                onMouseDown={(event) => this.onMouseDown(event)}>
+                <g transform={
+                    "scale(" + this.state.scale + ") " +
+                    "translate(" + this.state.translation.x + "," + this.state.translation.y + ") " +
+                    "rotate(" + this.state.rotation + ")"}>
+
+                    //Overlay
+                    <g/>
+                    //Svg contents
+                    <g dangerouslySetInnerHTML={{__html: this.state.inlineElements}}/>
+                </g>
+            </svg>
         );
     }
 }
